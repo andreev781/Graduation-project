88c51df7dd3bef2ec9976794a562fe44badf0cf2
Daniil Andreev <daniil03022006dan@gmail.com>
2020-05-17 21:06:49 +0500

    btnMore

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import formSubmisson from './modules/formSubmission';
 import modalWindow from './modules/modalWindowOne';
 import accordion from './modules/accordion';
 import calcAccordion from './modules/calcAccordion';
+import btnMore from './modules/btnMore';
 
 //modalWindowOne
 modalWindow('header .call-btn', '.popup');
@@ -25,3 +26,6 @@ modalWindow('.check-btn', '.popup-check');
 //calcAccordion
 
 calcAccordion();
+
+//btnMore
+btnMore();
---
+++ src/modules/btnMore.js
@@ -0,0 +1,14 @@
+'use strict';
+const btnMore = () => {
+    const btn = document.querySelector('.add-sentence-btn'),
+        blocks = document.querySelectorAll('.hidden');
+        btn.addEventListener('click', () => {
+        btn.style.display = 'none';
+        blocks.forEach((item) => {
+            console.log(item);
+            item.style.setProperty('display', 'block', 'important');
+        });
+    });
+};
+
+export default btnMore;
